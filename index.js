c7c3aaad25f93d6016c13bd1871f428f54b3ba9e
irfiyandaabidin <irfidupang@gmail.com>
2023-12-22 06:10:01 +0700

    add loging db

--- index.js
+++ index.js
@@ -6,8 +6,21 @@ const PORT = process.env.PORT || 4000;
 const MotorRoute = require("./routes/MotorRoute")
 const UserRoute = require("./routes/UserRoute")
 const cors = require("cors");
+const { Sequelize } = require('sequelize');
+const config = require('./config/database');
+
+const sequelize = new Sequelize(config.development);
 
 const app = express();
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
