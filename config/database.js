d60baa942913720fcfb20a42a084f7b8ca17bc68
irfiyandaabidin <irfidupang@gmail.com>
2023-12-22 05:47:19 +0700

    add env

---
+++ config/database.js
@@ -0,0 +1,41 @@
+module.exports = {
+  development: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: `${process.env.POSTGRES_DATABASE}-development`,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Set to false if using self-signed certificates
+      },
+    },
+  },
+  test: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: `${process.env.POSTGRES_DATABASE}-test`,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Set to false if using self-signed certificates
+      },
+    },
+  },
+  production: {
+    username: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
+    database: `${process.env.POSTGRES_DATABASE}-production`,
+    host: process.env.POSTGRES_HOST,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // Set to false if using self-signed certificates
+      },
+    },
+  },
+};
--- middleware/auth.js
+++ middleware/auth.js
@@ -23,3 +23,32 @@ module.exports = async (req, res, next) => {
     throw new Error("Internal Server Error");
   }
 };
+module.exports = async (req, res, next) => {
+  try {
+    const bearerToken = req.headers.authorization;
+
+    if (!bearerToken) {
+      throw new Error('You are unauthorized to make this request. Please log in.', 401);
+    }
+
+    const token = bearerToken.split('Bearer ')[1];
+    const payload = jwt.verify(token, 'secret');
+    const user = await User.findOne({
+      where: {
+        email: payload.email
+      }
+    });
+
+    if (!user) {
+      throw new Error('User not found.', 404);
+    }
+
+    req.user = user;
+    next();
+  } catch (err) {
+    console.error(err);  // Log the error for debugging purposes
+
+    // Return a more informative response to the client
+    res.status(err.status || 500).json({ error: err.message || 'Internal Server Error' });
+  }
+};
